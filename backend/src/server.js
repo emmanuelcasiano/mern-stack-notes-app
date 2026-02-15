76278d6dd09cc34da14e0ac5004554ac4ce48946
EC <casianoemmanuel2217@gmail.com>
2026-02-16 04:18:01 +0800

    Revert "add getNoteById controller"
    This reverts commit 2fd1aa16af68b5eb3d531fdc6f9ed8c28b1861a1.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,20 @@
 import express from "express";
 import notesRouter from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
+import { connectDB } from "../config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
 
 connectDB();
 
-// Middleware
-app.use(express.json());
-
 app.use("/api/notes", notesRouter);
 
-app.listen(PORT, () => {
-    console.log("Server running on PORT:", PORT);
+app.get("/api/notes", (req, res) => {
+    res.send("you got 5 notes3123");
+});
+
+app.listen(5001, () => {
+    console.log("Server running on port 5001");
 });
