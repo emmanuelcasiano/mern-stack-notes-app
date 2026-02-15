dd2880dde39f9ec1ee02d110cb3a740a175132e5
EC <casianoemmanuel2217@gmail.com>
2026-02-16 04:23:01 +0800

    implement getNoteById controller

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -9,6 +9,17 @@ export async function getAllNotes(_, res) {
     }
 }
 
+export async function getNoteById(req, res) {
+    try {
+        const note = await Note.findById(req.params.id);
+        if (!note) return res.status(404).json({ message: "Note not found" });
+        res.status(200).json(note);
+    } catch (error) {
+        console.error("Error in getNoteById controller", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
 export async function createNote(req, res) {
     try {
         const { title, content } = req.body;
