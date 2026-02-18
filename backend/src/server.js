8ddc74dec7df666813274e2ce7d19f9be109ab0d
EC <casianoemmanuel2217@gmail.com>
2026-02-18 23:41:09 +0800

    Implement some basic middleware and add dependencies for Redis

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,19 +2,27 @@ import express from "express";
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Middleware
+app.use(express.json()); // this middleware will parse the body of the request: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//     console.log(`Req method: ${req.method} & Req URL: ${req.url}`);
+//     next();
+// });
 
 app.use("/api/notes", notesRouter);
 
-app.get("/api/notes", (req, res) => {
-    res.send("you got 5 notes3123");
-});
-
 connectDB().then(() => {
-    app.listen(5001, () => {
-        console.log("Server running on port 5001");
+    app.listen(PORT, () => {
+        console.log("Server running on port", PORT);
     });
 });
